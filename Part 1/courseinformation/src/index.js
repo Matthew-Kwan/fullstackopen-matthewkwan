e7a725a280c314c0a5ec5f886033f451c25b85a8
Matthew Kwan <matthew.k913@gmail.com>
2020-09-02 16:15:22 -0400

    courseinformation complete

--- Part 1/courseinformation/src/index.js
+++ Part 1/courseinformation/src/index.js
@@ -1,75 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 // Exercise 1.1 refactor code so that it consists of 3 new components (Header, Content, and Total)
 
-const Header = (props) => {
-  return (
-    <div>
-      <h1>
-        {props.course}
-      </h1>
-    </div>
+// The {name,age} represent the destructuring of the props entered into Hello 
+const Hello = ({name, age}) => {
+
+    /* const bornYear = () => {
+      const yearNow = new Date().getFullYear()
+      return yearNow - props.age
+    } */
+    const bornYear = () => new Date().getFullYear() - age
+  
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old
+        </p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+    )
+  }
+  
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({ onClick, text }) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
-}
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part={props.parts[0].name} ex={props.parts[0].exercises} />
-      <Part part={props.parts[1].name} ex={props.parts[1].exercises} />
-      <Part part={props.parts[2].name} ex={props.parts[2].exercises} />
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <>
-      <p>
-        {props.part} {props.ex}
-      </p>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Number of exercise {props.ex1+props.ex2+props.ex3}
-      </p>
-    </div>
-  )
-}
-
-const App = () => {
-
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+const History = (props) => {
+    if (props.allClicks.length === 0) {
+      return (
+        <div>
+          the app is used by pressing the buttons
+        </div>
+      )
+    }
+  
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
   }
 
-  return (
+  
+const App = (props) => {
+const [value, setValue] = useState(10)
+
+return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+    {value}
+    <button>reset to zero</button>
     </div>
-  )
+)
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(
+<App />, 
+document.getElementById('root')
+)
