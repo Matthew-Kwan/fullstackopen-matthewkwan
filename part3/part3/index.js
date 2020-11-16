b4076e1b4e4a6b2a7cf004f5f32c0e2ebcb9f602
Matthew-Kwan <matthew.k913@gmail.com>
2020-11-15 20:09:47 -0500

    Finished first half of 3.1

--- part3/part3/index.js
+++ part3/part3/index.js
@@ -1,1 +1,91 @@
-console.log('hello world')
+// This is how Node.js imports modules
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let notes = [
+    {
+      id: 1,
+      content: "HTML is easy",
+      date: "2019-05-30T17:30:31.098Z",
+      important: true
+    },
+    {
+      id: 2,
+      content: "Browser can execute only Javascript",
+      date: "2019-05-30T18:39:34.091Z",
+      important: false
+    },
+    {
+      id: 3,
+      content: "GET and POST are the most important methods of HTTP protocol",
+      date: "2019-05-30T19:20:14.298Z",
+      important: true       
+    }
+  ] 
+
+// request contains all information of the http request
+// response is used to define how the request is responded to
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+
+// HTTP GET request for getting a single note 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id) 
+  const note = notes.find(note => note.id === id) 
+
+  if (note) {
+    response.json(note) 
+  } else {
+    response.status(404).end()
+  }
+})
+
+// HTTP DELETE request for deleting a single note 
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+// used to generate new Id for newly posted note
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+// HTTP POST request for adding a new note 
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
