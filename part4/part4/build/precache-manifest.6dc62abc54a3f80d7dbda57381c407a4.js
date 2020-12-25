a7b79fb4bc6af5f08a726516af143d1f0c63acb4
Matthew Kwan <matthew.k913@gmail.com>
2020-12-24 20:28:38 -0500

    Replicating notes app from Part 3 into part 4

---
+++ part4/part4/build/precache-manifest.6dc62abc54a3f80d7dbda57381c407a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c526cb799d3cfaacbebad071630552cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cb1d98f5521e216e13e",
+    "url": "/static/css/main.c722b9d8.chunk.css"
+  },
+  {
+    "revision": "69c6b86caf87f039ec3a",
+    "url": "/static/js/2.76c752df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.76c752df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb1d98f5521e216e13e",
+    "url": "/static/js/main.31842edd.chunk.js"
+  },
+  {
+    "revision": "4b4367ca4fbadb01e2d9",
+    "url": "/static/js/runtime-main.e5a25448.js"
+  }
+]);
---
+++ part4/part4/index.js
@@ -0,0 +1,106 @@
+// This is how Node.js imports modules
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const Note = require('./models/note')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.static('build'))
+
+// add in middleware error handler 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// request contains all information of the http request
+// response is used to define how the request is responded to
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+// HTTP GET request for getting all notes
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
+})
+
+// HTTP GET request for getting a single note
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+})
+
+
+// HTTP DELETE request for deleting a single note
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+// HTTP PUT request for updating importance of notes
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+// HTTP POST request for adding a new note
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note
+  .save()
+  .then(savedNote => savedNote.toJSON())
+  .then(savedAndFormattedNote => {
+    response.json(savedAndFormattedNote)
+  })
+  .catch(error => next(error))
+})
+
+// PORT definition
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
